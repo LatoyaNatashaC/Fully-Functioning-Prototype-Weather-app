fbde1f54c0dbb01011abc3cdc71b50157b31dca1
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-06-15 09:50:03 -0700

    Api is linked, temperature is displaying

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+function displayWeather(response) {
+  let temperatureElement = Math.round(response.data.temperature.current);
+  let displayTemperature = document.querySelector("#current-temperature");
+  displayTemperature.innerHTML = `${temperatureElement}`;
+}
+
+function searchCity(city) {
+  let apiKey = "4f3b0tf3219b4c7758082d0o48eabbbe";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
 function search(event) {
   event.preventDefault();
 
