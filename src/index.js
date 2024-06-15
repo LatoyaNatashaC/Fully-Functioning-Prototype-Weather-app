e1add5d0674298667a4ca874485f7deb64ddd3de
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-06-15 04:34:24 -0700

    Set h1/city to change with search input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 function search(event) {
   event.preventDefault();
+  let textInput = document.querySelector(".search-bar");
+
+  let h1 = document.querySelector("h1");
+  if (textInput.value) {
+    h1.innerHTML = `${textInput.value}`;
+  } else {
+    h1.innerHTML = null;
+  }
 }
 
 let searchButton = document.querySelector(".search-button");
